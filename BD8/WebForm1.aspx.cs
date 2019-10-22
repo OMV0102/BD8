b01a98dcd58f30647a2dd2d317f25dad313e83e5
omv10 <omv98@outlook.com>
2019-10-22 21:56:35 +0700

    Добавьте файлы проекта.

---
+++ BD8/WebForm1.aspx.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Odbc;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+
+
+namespace BD8
+{
+    public partial class WebForm1 : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+            OdbcConnection conn = new OdbcConnection(); // Создаем объект подключения
+            conn.ConnectionString = "Dsn=PostgreSQL16"; // Задаем параметр подключения – имя ODBC-источника
+            conn.Open(); // Подключаемся к БД
+        }
+    }
+}
